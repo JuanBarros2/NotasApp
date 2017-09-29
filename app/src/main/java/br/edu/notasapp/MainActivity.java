c6d0221e4b665b2665c72bfb6402383320e61b6a
Juan Barros <juanlyrabarros@gmail.com>
2017-09-29 02:03:34 -0300

    Aula 2 Finalizada

--- app/src/main/java/br/edu/notasapp/MainActivity.java
+++ app/src/main/java/br/edu/notasapp/MainActivity.java
@@ -2,12 +2,33 @@ package br.edu.notasapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+    private EditText campoNotas;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        campoNotas = (EditText) findViewById(R.id.editText);
+
+    }
+
+    public void onBtnClick(View v){
+        String notas = campoNotas.getText().toString();
+
+        Log.d("Log", notas);
+        /* Utilizamos para mostrar mensagens de Log. Será tratado com mais detalhes
+        posteriormente
+        */
+        Toast.makeText(this, notas, Toast.LENGTH_LONG).show();
+        /*Não se preocupe com esse comando, ele apenas mostra uma mensagem
+        na tela sem precisar de um componente específico para isso.
+         */
     }
 }
